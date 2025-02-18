f32459808c4666ed066dba9c8117a2df66f254aa
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-02-18 18:23:51 +0600

    git init

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import { useEffect } from 'react';
+
 import './App.css';
+import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
     const {onToggleButton, tg} = useTelegram();
 
-    useEffect( () => {
+    useEffect(() => {
         tg.ready();
     }, [])
 
-
     return (
         <div className="App">
-
-            <button onClick={onToggleButton}>Close</button>
-
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
         </div>
     );
 }

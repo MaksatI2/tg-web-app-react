3d19e708eb75f1f5dbc7e5a100d7ae58ce644db9
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-02-18 12:36:42 +0600

    git init

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     return (
         <div className="App">
 
-            <button onClick={onToggleButton}>toggle</button>
+            <button onClick={onToggleButton}>Close</button>
 
         </div>
     );
